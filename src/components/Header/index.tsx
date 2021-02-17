368394eb3a49cdd8ed4d14bc0d87aab304700411
Otavio Ho Aragoni <otavio.aragoni@gmail.com>
2021-02-16 21:01:41 -0300

    Header style

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -6,7 +6,7 @@ import logo from "../../assets/logo.svg";
 export default function Header(props: any) {
   return (
     <header>
-      <img src={logo} alt="Real Trends Logo" id="logo" />
+      {/* <img src={logo} alt="Real Trends Logo" id="logo" /> */}
       <h1>To Do List</h1>
     </header>
   );
